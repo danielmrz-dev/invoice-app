a083b9f77b0cf4e1c2026c385ef0522a3cc54828
Daniel Mariz <danielmrz.dev@gmail.com>
2025-05-12 11:07:05 -0300

    Cria interface e um arquivo json com os dados do projeto

--- src/app/models/invoice.interface.ts
+++ src/app/models/invoice.interface.ts
@@ -1,3 +1,35 @@
 export interface Invoice {
-    id: number;
+    id: string;
+    createdAt: string;
+    paymentDue: string;
+    description: string;
+    paymentTerms: number;
+    clientName: string;
+    clientEmail: string;
+    status: string;
+    senderAddress: ISenderAddress;
+    clientAddress: IClientAddress;
+    items: Item[];
+    total: number;
+}
+
+export interface ISenderAddress {
+    street: string;
+    city: string;
+    postCode: string;
+    country: string;
+}
+
+export interface IClientAddress {
+    street: string;
+    city: string;
+    postCode: string;
+    country: string;
+}
+
+export interface Item {
+    name: string;
+    quantity: number;
+    price: number;
+    total: number;
 }
