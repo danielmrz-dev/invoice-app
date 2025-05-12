9f3a7b39b717d89f73b33c47d6a0156f28667278
Daniel Mariz <danielmrz.dev@gmail.com>
2025-05-12 11:49:59 -0300

    Cria componente header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { InvoicesListComponent } from "./components/invoices-list/invoices-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InvoicesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
